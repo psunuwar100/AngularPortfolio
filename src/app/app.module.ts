bec97e8580ba007b04875efde31124e2a0509728
psunuwar100 <psunuwar100@gmail.com>
2024-07-10 17:50:21 +0545

    design complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { DetailsComponent } from './components/details/details.component';
+import { ProjectComponent } from './components/project/project.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    IntroComponent,
+    DetailsComponent,
+    ProjectComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
